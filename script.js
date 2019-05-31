a3abfba2cad1be260a55d5976ec28f45cdd49f00
szczepaniakmichal <mssk87@gmail.com>
2019-05-31 15:53:59 +0200

    draw table, ball, player, ai,
    ball is in test version

---
+++ script.js
@@ -0,0 +1,63 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 1000;
+canvas.height = 500;
+
+const cw = canvas.width;
+const ch = canvas.height;
+
+const ballSize = 20;
+let ballX = cw/2-ballSize/2;
+let ballY = ch/2-ballSize/2;
+
+const paddleHeight = 100;
+const paddleWitdht = 20;
+
+const playerX = 30;
+const aiX = 940;
+
+let playerY = 200;
+let aiY = playerY;
+
+// draw a table
+function table() {
+    ctx.fillStyle = 'rgb(0, 255, 0, 0.767)';
+    ctx.fillRect(0,0,cw,ch)
+}
+
+// // draw the ball
+// function ball() {
+//     ctx.fillStyle = 'white';
+//     ctx.fillRect(ballX, ballY, ballSize, ballSize);
+// }
+
+// draw the ball
+function ball() {
+    ctx.fillStyle = 'white';
+    ctx.arc(ballX, ballY, ballSize, ballSize, Math.PI*2,true);
+    ctx.stroke();
+}
+
+// draw a player
+function player() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(playerX, playerY, paddleWitdht, paddleHeight);
+}
+   
+// draw a ai
+function ai() {
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(aiX, aiY, paddleWitdht, paddleHeight);
+}
+
+// draw all funcion
+function start() {
+    table();
+    
+    player();
+    ai();
+    ball();
+}
+
+start();
