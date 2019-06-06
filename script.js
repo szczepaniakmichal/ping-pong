39b06f8beed535ec13bcddf081bf6f9a2cc05508
szczepaniakmichal <mssk87@gmail.com>
2019-06-06 07:55:53 +0200

    update draw the ball, add aiPosition()

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ canvas.height = 500;
 const cw = canvas.width;
 const ch = canvas.height;
 
-const ballSize = 20;
+const ballSize = 10;
 let ballX = cw/2-ballSize/2;
 let ballY = ch/2-ballSize/2;
 
@@ -42,9 +42,26 @@ function table() {
 }
 
 //draw the square ball
+// function ball() {
+//     ctx.fillStyle = 'white';
+//     ctx.fillRect(ballX, ballY, ballSize, ballSize);
+//     ballX += ballSpeedX;
+//     ballY += ballSpeedY;
+//     if ( ballY <= 0 || ballY + ballSize >= ch ){
+//         ballSpeedY = -ballSpeedY;
+//         speedUp();
+//     }
+//     if ( ballX <= 0 || ballX + ballSize >= cw ){
+//         ballSpeedX = -ballSpeedX;
+//     }
+//  }
+
+// draw the ball
 function ball() {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(ballX, ballY, ballSize, ballSize);
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, ballSize, 0, Math.PI*2, true);
+    ctx.fillStyle = "yellow";
+    ctx.fill(); 
     ballX += ballSpeedX;
     ballY += ballSpeedY;
     if ( ballY <= 0 || ballY + ballSize >= ch ){
@@ -54,18 +71,10 @@ function ball() {
     if ( ballX <= 0 || ballX + ballSize >= cw ){
         ballSpeedX = -ballSpeedX;
     }
- }
-
-// draw the ball
-// function ball() {
-//     ctx.beginPath();
-//     ctx.arc(ballX, ballY, ballSize, 0, Math.PI*2, true);
-//     ctx.fillStyle = "yellow";
-//     ctx.fill(); 
     //ctx.lineWidth = 5;  //error const lineWidth = 6; in line 23. 
     //ctx.strokeStyle = "red";
     //ctx.stroke();
-//}
+}
 
 // draw a player
 function player() {
@@ -91,29 +100,56 @@ function playerPosition(e) {
 }
 
 function aiPosition() {
+
+    const middlePaddle = aiY + paddleHeight/2;
+    const middlBall = ballY + ballSize/2;
+
     if ( ballX > 500 ) {
-
+       if (middlePaddle - middlBall > 200){
+        aiY -= 30;
+       } else if (middlePaddle - middlBall > 50){
+        aiY -=5;
+       }
+       else if (middlePaddle - middlBall < -200){
+        aiY +=15;
+    }  else if (middlePaddle - middlBall < -50){
+        aiY += 5;
     }
-
     if ( ballX < 500 && ballX > 150) {
-
+        if (middlePaddle - middlBall > 100){
+            aiY -= 3;
+        } else if (middlePaddle - middlBall < -100){
+            aiY +=3;
+        }
     }
 
+    //my code - works
+    // if ( ballX < 500 && ballX > 150) {
+    //     aiY = ballY - paddleHeight/2 + ballSize/2;
+    // }
+    // if ( ballX > 500 ) {
+    //     aiY = ballY - paddleHeight/2 + ballSize/2;
+    // }
+
+    // if ( ballX < 500 && ballX > 150) {
+    //     aiY = ballY - paddleHeight/2 + ballSize/2;
+    // }
+}
 }
 
 function speedUp() {
-    if ( ballSpeedX > 0 && ballSpeedX < 16 ) {
-        ballSpeedX += .4;
+    if ( ballSpeedX > 0 && ballSpeedX < 4 ) {
+        ballSpeedX += .1;
         console.log(ballSpeedX);
-    } else if ( ballSpeedX < 0 && ballSpeedX > -16 ) {
-        ballSpeedX -= .4;
+    } else if ( ballSpeedX < 0 && ballSpeedX > -4 ) {
+        ballSpeedX -= .1;
         console.log(ballSpeedX);
     }
 
-    if ( ballSpeedY > 0 && ballSpeedY <16 ) {
-        ballSpeedY += .3;
-    } else if ( ballSpeedY < 0 && ballSpeedY > -16 ) {
-        ballSpeedY -=3;
+    if ( ballSpeedY > 0 && ballSpeedY <4 ) {
+        ballSpeedY += .1;
+    } else if ( ballSpeedY < 0 && ballSpeedY > -4 ) {
+        ballSpeedY -=1;
     }
 }
 
@@ -126,7 +162,6 @@ function start() {
     ai();
     ball();
     aiPosition();
-    //setInterval(ball, 60);
 }
 
 setInterval(start, 1000 / 60);
